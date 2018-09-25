b656c37468af7659eb1ccfea712bc12f5e32c06d
Trey Miller <Trey_M@heitkamp.corp>
2018-09-25 13:45:02 -0500

    Minor Update - Added Function - Added a, d flag conditionals

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 #! /usr/bin/env/python3
-import argparse, re, sys
+import argparse
+import re
+import sys  
+import os
+from pathlib import Path
 
 # handle arguments
 parser = argparse.ArgumentParser(
     description='A tree clone written in python. This program will take a directory path and recursively print all sub folders and files')
 
-parser.add_argument('directory', help='directory to parse') 
-parser.add_argument('-a', help='All files are printed, including hidden ones')
-parser.add_argument('-d', help='List directories only')
-parser.add_argument('-f', help='Prints the full path prefix for each file') 
+parser.add_argument('directory', help='directory to parse', action='store') 
+parser.add_argument('-a',  help='All files are printed, including hidden ones', action='store_true')
+parser.add_argument('-d', help='List directories only', action='store_true')
+parser.add_argument('-f', help='Prints the full path prefix for each file', action='store_true') 
 parser.add_argument('-v', '--verbose', help='log more verbosely', action='store_true')
 
 args = parser.parse_args() 
+
+directory = Path(args.directory)
  
- 
+def printSubDirectories(path):
+    for child in path.iterdir():
+        if child.is_dir():
+            if str(child)[0] == '.':
+                if args.a:
+                    print(child.resolve())
+                    printSubDirectories(child)
+            else:
+                print(child.resolve())
+                printSubDirectories(child)
+        elif child.is_file and not args.d:
+            print(child.resolve())
+
+
+if directory.exists():
+    printSubDirectories(directory)
